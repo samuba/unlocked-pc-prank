63e68cedd5b13e8f90015fabf733adee55c4523a
Samuel Bach <samuel.z.bach@gmail.com>
2015-02-12 21:17:30 +0100

    initial commit

---
+++ unlocked-pc-prank/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace unlocked_pc_prank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("PC is unlocked? We have a job for you Mr. Powers!");
+
+            System.Console.WriteLine("Deploying Special Agent Austin Powers...");
+            Wallpaper.Set(Resources.dangerously, Wallpaper.Style.Stretched);
+
+            System.Console.WriteLine("Austin was delivered - Mission succesfull");
+            System.Console.WriteLine("Press any key to exit and leave the scene quietly...");
+
+            System.Console.ReadLine();
+        }
+    }
+}
---
+++ unlocked-pc-prank/Wallpaper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.IO;
+using Microsoft.Win32;
+
+namespace unlocked_pc_prank
+{
+    public sealed class Wallpaper
+    {
+        Wallpaper() { }
+
+        const int SPI_SETDESKWALLPAPER = 20;
+        const int SPIF_UPDATEINIFILE = 0x01;
+        const int SPIF_SENDWININICHANGE = 0x02;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+        public enum Style : int
+        {
+            Tiled,
+            Centered,
+            Stretched
+        }
+
+        public static void Set(Image img, Style style)
+        {
+            var tempPath = Path.Combine(Path.GetTempPath(), "wallpaper.bmp");
+            img.Save(tempPath, System.Drawing.Imaging.ImageFormat.Bmp);
+
+            var key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
+            if (style == Style.Stretched)
+            {
+                key.SetValue(@"WallpaperStyle", 2.ToString());
+                key.SetValue(@"TileWallpaper", 0.ToString());
+            }
+
+            if (style == Style.Centered)
+            {
+                key.SetValue(@"WallpaperStyle", 1.ToString());
+                key.SetValue(@"TileWallpaper", 0.ToString());
+            }
+
+            if (style == Style.Tiled)
+            {
+                key.SetValue(@"WallpaperStyle", 1.ToString());
+                key.SetValue(@"TileWallpaper", 1.ToString());
+            }
+
+            SystemParametersInfo(SPI_SETDESKWALLPAPER,
+                0,
+                tempPath,
+                SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+        }
+    }
+}
